49d611cb9e2862c9bc6f088ef7026bce109970f4
Josh Breytspraak <133163031+JBreyt24@users.noreply.github.com>
2023-10-19 00:42:22 -0500

    Add files via upload

---
+++ Hover_Event_Practice/script.js
@@ -0,0 +1,9 @@
+console.log("yo I'm working")
+
+function addShadow(element) {
+    element.classList.add("shadow");
+}
+
+function removeShadow(element) {
+    element.classList.remove("shadow");
+}
